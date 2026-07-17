58e1e093d77982e39bc47be09bdba7915a47cdfa
khushigarg05 <gargkhushi0511@gmail.com>
2026-07-17 10:25:54 +0530

    hoisting concept

--- day_0/index.js
+++ day_0/index.js
@@ -1,5 +1,17 @@
-console.log(a);
+/*console.log(a);
 var a=2;
 console.log(typeof(a));
 var a="abc";
 console.log(type)
+*/
+
+//hoisting
+console.log(a);
+var a;
+a=2;
+{
+    var a ="2";
+    a= 2+ "2";
+    console.log(a);
+}
+console.log(a);
