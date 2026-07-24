410f514fcb3ef32f64eb0931273b7ed4f7dcfb77
khushigarg05 <gargkhushi0511@gmail.com>
2026-07-24 09:31:43 +0530

    function scoping concept

---
+++ day_1/block.js
@@ -0,0 +1,13 @@
+let a=34;
+console.log(34);
+{
+    //console.log(a);
+    let a=45;
+    console.log(a);
+    let b=56;
+    console.log(b);
+    a=78;
+    console.log(a);
+}
+console.log(a);
+//console.log(b);
---
+++ day_1/functionScoping.js
@@ -0,0 +1,11 @@
+greet();
+console.log(greet);
+function greet(){
+    let section = "cse18";
+    //var subject ="FSD";
+    console.log('${section} ${subject}');
+}
+greet();
+console.log(greet);
+
+//console.log(subject);
